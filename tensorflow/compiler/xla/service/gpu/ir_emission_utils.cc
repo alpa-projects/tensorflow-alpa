3e6332a6ed89697f6224a4589a61a6fdcae64da7
Yonghao ZHUANG <45167880+ZYHowell@users.noreply.github.com>
2021-06-14 12:27:54 +0800

    Swap runtime with overlap supported (#12)
    * Add swap insertion pass
    
    * Add swap insertion runtime
    
    * turn to swap thunk
    
    * update
    
    * workable overlap
    
    * improve code style, remove done todo
    
    * update

--- tensorflow/compiler/xla/service/gpu/ir_emission_utils.cc
+++ tensorflow/compiler/xla/service/gpu/ir_emission_utils.cc
@@ -211,6 +211,9 @@ bool ImplementedAsLibraryCall(const HloInstruction& hlo) {
          IsCustomCallToDnnConvolution(hlo);
 }
 
+const char* const kBuiltinSwapOutTarget = "__builtin$SwapOut";
+const char* const kBuiltinSwapInTarget = "__builtin$SwapIn";
+
 static ReductionDimensions GetReductionKindAndContiguousComponentsImpl(
     const Shape& input_shape, absl::Span<const int64_t> dims_to_reduce) {
   DimensionVector dims_to_keep;

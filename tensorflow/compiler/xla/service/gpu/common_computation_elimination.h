d8463606ff1d6f754bd763b27c3dd3c94dcbb5c1
Lianmin Zheng <lianminzheng@gmail.com>
2021-11-25 20:25:13 +0000

    eliminate common subcomputation

---
+++ tensorflow/compiler/xla/service/gpu/common_computation_elimination.h
@@ -0,0 +1,25 @@
+#ifndef TENSORFLOW_COMPILER_XLA_SERVICE_GPU_COMMON_COMPUTATION_ELIMINATION_H_
+#define TENSORFLOW_COMPILER_XLA_SERVICE_GPU_COMMON_COMPUTATION_ELIMINATION_H_
+
+#include "absl/strings/string_view.h"
+#include "tensorflow/compiler/xla/service/hlo_module.h"
+#include "tensorflow/compiler/xla/service/hlo_pass_interface.h"
+
+namespace xla {
+namespace gpu {
+
+// Eliminate common subcomputation
+class CommonComputationElimination : public HloModulePass {
+ public:
+  CommonComputationElimination() = default;
+  ~CommonComputationElimination() override = default;
+
+  absl::string_view name() const override { return "common-computation-elimination"; }
+
+  StatusOr<bool> Run(HloModule* module) override;
+};
+
+}  // namespace gpu
+}  // namespace xla
+
+#endif  // TENSORFLOW_COMPILER_XLA_SERVICE_GPU_COMMON_COMPUTATION_ELIMINATION_H_

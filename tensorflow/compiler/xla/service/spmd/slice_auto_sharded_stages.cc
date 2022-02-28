cceb2f0dc4ce0542dee359c8020ff13bc71c56cb
Zhuohan Li <zhuohan123@gmail.com>
2022-02-27 21:30:14 -0800

    remove old code

--- tensorflow/compiler/xla/service/spmd/slice_auto_sharded_stages.cc
+++ tensorflow/compiler/xla/service/spmd/slice_auto_sharded_stages.cc
@@ -18,36 +18,7 @@ enum VisitState { kVisiting, kVisited };
 constexpr absl::string_view kPipelineMarkerStartType = "start";
 constexpr absl::string_view kPipelineMarkerEndType = "end";
 
-std::vector<HloInstruction*> GetAncestorInstructions(
-    HloInstruction* start_ins) {
-  std::vector<HloInstruction*> postorder;
-  absl::flat_hash_map<HloInstruction*, VisitState> visited;
-  std::vector<HloInstruction*> dfs_stack;
-  dfs_stack.push_back(start_ins);
-
-  while (!dfs_stack.empty()) {
-    auto* cur = dfs_stack.back();
-    auto it = visited.find(cur);
-    if (it != visited.end()) {
-      dfs_stack.pop_back();
-      if (it->second == kVisited) {
-        continue;
-      }
-      CHECK_EQ(it->second, kVisiting);
-      postorder.push_back(cur);
-      it->second = kVisited;
-      continue;
-    }
-
-    visited.insert({cur, kVisiting});
-    for (HloInstruction* operand : cur->operands()) {
-      dfs_stack.push_back(operand);
-    }
-  }
-  return postorder;
-}
-
-std::unique_ptr<HloModule> CreateStageModuleDFS(
+std::unique_ptr<HloModule> CreateStageModule(
     HloModule* full_module, HloInstruction* stage_start_instruction,
     HloInstruction* stage_end_instruction, std::string stage_name_suffix) {
   CHECK(stage_start_instruction->IsCustomCall(kXlaPipelineMarker));
@@ -140,8 +111,8 @@ std::unique_ptr<HloModule> CreateStageModuleDFS(
   }
 
   // Build the HLO computation.
-  HloComputation::Builder builder(
-      absl::StrCat(full_module->entry_computation()->name(), "-", stage_name_suffix));
+  HloComputation::Builder builder(absl::StrCat(
+      full_module->entry_computation()->name(), "-", stage_name_suffix));
   for (auto& ins : instructions) {
     builder.AddInstruction(std::move(ins));
   }
@@ -161,124 +132,6 @@ std::unique_ptr<HloModule> CreateStageModuleDFS(
   module->AddEntryComputationWithLayouts(std::move(new_computation));
 
   return module;
-}
-
-std::unique_ptr<HloModule> CreateStageModule(
-    HloModule* full_module,
-    const std::vector<HloInstruction*>& stage_instructions,
-    std::string suffix) {
-  HloModuleConfig config = full_module->config();
-  // TODO (zhuohan): Support input/output alias
-  config.set_shardable_value_update_pairs({});
-  config.mutable_fusion_config()->clear();
-  config.mutable_dot_config()->clear();
-  config.mutable_layout_config()->clear();
-
-  auto module = absl::make_unique<HloModule>(
-      absl::StrCat(full_module->name(), "-", suffix), config);
-
-  std::unique_ptr<HloCloneContext> context_ptr =
-      absl::make_unique<HloCloneContext>(module.get(), suffix);
-  HloCloneContext* context = context_ptr.get();
-
-  std::vector<std::unique_ptr<HloInstruction>> instructions;
-
-  HloInstruction* stage_start_instruction = stage_instructions.front();
-  HloInstruction* stage_end_instruction = stage_instructions.back();
-  CHECK(stage_start_instruction->IsCustomCall(kXlaPipelineMarker));
-  CHECK(stage_end_instruction->IsCustomCall(kXlaPipelineMarker));
-
-  CHECK(stage_start_instruction->shape().IsTuple());
-  size_t n_parameters = stage_start_instruction->shape().tuple_shapes_size();
-  std::vector<HloInstruction*> parameters(n_parameters);
-  for (size_t i = 0; i < n_parameters; ++i) {
-    auto new_param = HloInstruction::CreateParameter(
-        i, stage_start_instruction->shape().tuple_shapes(i),
-        absl::StrCat("param_", i));
-    if (stage_start_instruction->has_sharding()) {
-      CHECK(stage_start_instruction->sharding().IsTuple());
-      new_param->set_sharding(
-          stage_start_instruction->sharding().GetSubSharding(
-              stage_start_instruction->shape(), {i}));
-    }
-    new_param->set_metadata(stage_start_instruction->metadata());
-    parameters[i] = new_param.get();
-    instructions.push_back(std::move(new_param));
-  }
-
-  // std::cerr << "======old instructions=====" << std::endl;
-  for (size_t i = 1; i < stage_instructions.size() - 1; ++i) {
-    HloInstruction* ins = stage_instructions[i];
-    if (ins->opcode() == HloOpcode::kParameter) {
-      continue;
-    }
-
-    // std::cerr << ins->ToString() << std::endl;
-    if (ins->opcode() == HloOpcode::kGetTupleElement &&
-        ins->operand(0) == stage_start_instruction) {
-      int64_t param_no = ins->tuple_index();
-      context->MapInstruction(ins, parameters[param_no]);
-    } else {
-      std::vector<HloInstruction*> new_operands;
-      for (auto operand : ins->operands()) {
-        HloInstruction* new_operand = context->FindInstruction(operand);
-        if (new_operand == nullptr) {
-          std::vector<HloInstruction*> ancestors =
-              GetAncestorInstructions(operand);
-          for (auto ancestor : ancestors) {
-            // Make sure the ancestor is a constant
-            // TODO (zhuohan): Might also check that the opcode is not
-            // kRngGetAndUpdateState
-            CHECK_NE(ancestor->opcode(), HloOpcode::kParameter);
-            std::vector<HloInstruction*> new_ancestor_operands;
-            for (auto ancestor_operand : ancestor->operands()) {
-              new_ancestor_operands.push_back(
-                  context->GetInstruction(ancestor_operand));
-            }
-            instructions.push_back(ancestor->CloneWithNewOperands(
-                ancestor->shape(), new_ancestor_operands, context));
-          }
-          new_operand = context->GetInstruction(operand);
-        }
-        new_operands.push_back(new_operand);
-      }
-      instructions.push_back(
-          ins->CloneWithNewOperands(ins->shape(), new_operands, context));
-    }
-  }
-  // std::cerr << "======new instructions=====" << std::endl;
-  // for (const auto &ins : instructions) {
-  //   std::cerr << ins->ToString() << std::endl;
-  // }
-  HloComputation::Builder builder(
-      absl::StrCat(full_module->entry_computation()->name(), "-", suffix));
-  for (auto& ins : instructions) {
-    builder.AddInstruction(std::move(ins));
-  }
-  std::unique_ptr<HloComputation> new_computation = builder.Build(
-      /*root_instruction=*/context->GetInstruction(
-          stage_end_instruction->operand(0)));
-
-  for (size_t i = 1; i < stage_instructions.size() - 1; ++i) {
-    if (stage_instructions[i]->opcode() == HloOpcode::kParameter) {
-      continue;
-    }
-
-    HloInstruction* ins = stage_instructions[i];
-    HloInstruction* new_ins = context->GetInstruction(ins);
-    for (auto successor : ins->control_successors()) {
-      TF_CHECK_OK(
-          new_ins->AddControlDependencyTo(context->GetInstruction(successor)));
-    }
-  }
-
-  // std::cerr << "======new computation=====" << std::endl;
-  // std::cerr << new_computation->ToString() << std::endl;
-
-  // NOTE: We assume the HLO graph only has one computation.
-  module->AddEntryComputationWithLayouts(std::move(new_computation));
-
-  return std::move(module);
 }
 
 std::vector<std::string> HookShardingProto(HloModule* module) {
@@ -322,9 +175,9 @@ std::vector<std::unique_ptr<HloModule>> SliceAutoShardedStagesInternal(
         CHECK_EQ(current_ins->metadata().op_name(), current_stage_name);
         CHECK_EQ(current_ins->metadata().op_type(), kPipelineMarkerEndType);
         pipeline_stage_names.push_back(current_stage_name);
-        pipeline_stages.push_back(
-            CreateStageModuleDFS(module, current_stage_start, current_stage_end,
-                                 current_stage_name));
+        pipeline_stages.push_back(CreateStageModule(module, current_stage_start,
+                                                    current_stage_end,
+                                                    current_stage_name));
         current_stage_name.clear();
         in_stage = false;
       } else {

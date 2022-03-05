0117b5e7043f7db09b0f3bf8df3f96e154ce8c84
Lianmin Zheng <lianminzheng@gmail.com>
2022-03-05 03:25:07 -0800

    Reorganize passes and build options (#95)
    * reorganize passes
    
    * reorganize passes

--- tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
@@ -329,13 +329,20 @@ Status GpuCompiler::OptimizeHloModule(
             });
         spmd_simplify.AddPass<HloCSE>(/*is_layout_sensitive=*/false);
         spmd_simplify.AddPass<HloDCE>();
+
+        if (pass_context::GetBool("build_option::run_auto_sharding", false)) {
+          spmd_pipeline.AddPass<xla::spmd::AutoSharding>();
+          spmd_pipeline.AddPass<xla::spmd::SliceAutoShardedStages>();
+        }
+
       }
 
-      spmd_pipeline.AddPass<xla::spmd::AutoSharding>();
-      spmd_pipeline.AddPass<xla::spmd::SliceAutoShardedStages>();
-      spmd_pipeline.AddPass<ShardingPropagation>(/*is_spmd=*/true);
-      spmd_pipeline.AddPass<GpuSpmdPartitioner>(
-          num_partitions, hlo_module->config().replica_count());
+      if (pass_context::GetBool("build_option::run_spmd_partitioner", true)) {
+        spmd_pipeline.AddPass<ShardingPropagation>(/*is_spmd=*/true);
+        spmd_pipeline.AddPass<GpuSpmdPartitioner>(
+            num_partitions, hlo_module->config().replica_count());
+        spmd_pipeline.AddPass<xla::spmd::GradAccRewrite>();
+      }
     } else {
       spmd_pipeline.AddPass<xla::spmd::SliceAutoShardedStages>();
       // Remove redundant sharding ops when partition_count == 1.
@@ -344,7 +351,7 @@ Status GpuCompiler::OptimizeHloModule(
     }
     TF_RETURN_IF_ERROR(spmd_pipeline.Run(hlo_module).status());
 
-    if (pass_context::GetBool("build_option::return_after_slice_auto_sharded_stages", false)) {
+    if (!pass_context::GetBool("build_option::run_post_spmd_partitioner_passes", true)) {
       return Status::OK();
     }
   }
@@ -513,7 +520,6 @@ Status GpuCompiler::OptimizeHloModule(
     collectives_pipeline.AddPass<ReduceScatterCreator>();
     collectives_pipeline.AddPass<RematIdentityFixer>();
     collectives_pipeline.AddPass<AllReduceReassociate>();
-    collectives_pipeline.AddPass<xla::spmd::GradAccRewrite>();
 
     // Run algebraic simplifier to reshape(broadcast) into a broadcast when
     // the reshape is just adding a unit dimension. This will help with the
@@ -776,12 +782,6 @@ Status GpuCompiler::OptimizeHloPostLayoutAssignment(
 StatusOr<std::unique_ptr<HloModule>> GpuCompiler::RunHloPasses(
     std::unique_ptr<HloModule> module, se::StreamExecutor* stream_exec,
     const CompileOptions& options) {
-  if (!pass_context::GetBool("build_option::run_hlo_passes", true)) {
-    // Do no run the HLO optimization passes. Assume the input HloModule
-    // has already been optimized.
-    return std::move(module);
-  }
-
   // We dump the post-optimization HLO in RunBackend so no need to dump it here.
   XLA_SCOPED_LOGGING_TIMER("GpuCompiler::RunHloPasses");
   tensorflow::profiler::TraceMe activity(
@@ -1191,7 +1191,7 @@ GpuCompiler::CompileToTargetBinary(const HloModuleConfig& module_config,
 StatusOr<std::unique_ptr<Executable>> GpuCompiler::RunBackend(
     std::unique_ptr<HloModule> module, se::StreamExecutor* stream_exec,
     const CompileOptions& options) {
-  if (!pass_context::GetBool("build_option::run_backend_codegen", true)) {
+  if (!pass_context::GetBool("build_option::run_post_spmd_partitioner_passes", true)) {
     // Do no run backend code generation. Return a dummy executable.
     GpuExecutable::Params params;
     params.debug_module = std::move(module);

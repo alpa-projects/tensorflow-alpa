8d5b902568a8fc8641b35284ef8630f7b83c581a
ZYHowell <zhuangyh@sjtu.edu.cn>
2022-03-26 05:48:58 +0000

    add passes

--- tensorflow/compiler/xla/service/spmd/alpa_compile.h
+++ tensorflow/compiler/xla/service/spmd/alpa_compile.h
@@ -6,7 +6,7 @@ namespace spmd {
 
 // Compile an Xla Computation into HloModule, then apply Alpa's passes.
 // The result hlo is later compiled again to apply spmd and other optimizations.
-StatusOr<std::shared_ptr<xla::HloModule>> AlpaCompile(
+StatusOr<std::unique_ptr<xla::HloModule>> AlpaCompile(
     const XlaComputation& computation, CompileOptions options);
 
 };  // namespace spmd
